31b7db905652460c7f62494ed1c5e0119ce32bfc
xuke <825126369@qq.com>
2017-01-22 05:21:54 +0800

    2017.1.22 年终提交

--- Tools/Linux_Res/Linux_Study/makefile/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "test.h"
-using namespace std;
-int main()
-{
-	cout<<"sdfsdf"<<endl;
-	bbb();
-	return 0;
-}
--- Tools/Linux_Res/Linux_Study/makefile/src/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "test.h"
-using namespace std;
-
-void bbb()
-{
-	cout<<"This is Name \"test.cpp\""<<endl;
-}
-
